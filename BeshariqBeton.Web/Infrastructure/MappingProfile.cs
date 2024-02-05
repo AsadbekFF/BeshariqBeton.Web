317183841a0d0146d9e0d688880cb85886e5ea93
Asadbek Fayziev <asadbek.fayziev1507@gmail.com>
2024-02-05 13:59:28 +0500

    Fix sales come in time allow nullable.

--- BeshariqBeton.Web/Infrastructure/MappingProfile.cs
+++ BeshariqBeton.Web/Infrastructure/MappingProfile.cs
@@ -21,7 +21,10 @@ namespace BeshariqBeton.Web.Infrastructure
             CreateMap<PriceSettingsViewModel, SumpPiecesPricesParameters>().ReverseMap();
             CreateMap<StorageViewModel, StorageParameters>().ReverseMap();
             CreateMap<ClientViewModel, Client>().ReverseMap();
-            CreateMap<SaleViewModel, Sale>().ReverseMap();
+            CreateMap<SaleViewModel, Sale>()
+                .ForMember(s => s.ComeInDateTime, m => m.MapFrom(c => c.ComeOutDateTime.Date + c.ComeInTime))
+                .ReverseMap()
+                .ForMember(s => s.ComeInTime, m => m.MapFrom(c => c.ComeInDateTime.Value.TimeOfDay));
             CreateMap<StorageViewModel, Storage>().ReverseMap();
         }
     }
--- BeshariqBeton.Web/ViewModels/SaleViewModel.cs
+++ BeshariqBeton.Web/ViewModels/SaleViewModel.cs
@@ -1,5 +1,4 @@
-﻿using BeshariqBeton.Common.Entities;
-using BeshariqBeton.Common.Enums;
+﻿using BeshariqBeton.Common.Enums;
 using System.ComponentModel.DataAnnotations;
 
 namespace BeshariqBeton.Web.ViewModels
@@ -8,22 +7,31 @@ namespace BeshariqBeton.Web.ViewModels
     {
         [Display(Name = "Klient")]
         public int ClientId { get; set; }
+
         [Display(Name = "Beton turi")]
         public ConcreteProductType ConcreteProductType { get; set; }
+
         [Display(Name = "Miqdori")]
         public int? Count { get; set; }
+
         [Display(Name = "Chiqib ketgan vaqti")]
         public DateTime ComeOutDateTime { get; set; }
+
         [Display(Name = "Qaytib kelgan vaqti")]
-        public DateTime? ComeInDateTime { get; set; }
+        public TimeSpan? ComeInTime { get; set; }
+
         [Display(Name = "Pastgi qismi miqdori")]
         public int? BottomCount { get; set; }
+
         [Display(Name = "60 lik miqdori")]
         public int? Sump60Count { get; set; }
+
         [Display(Name = "90 lik miqdori")]
         public int? Sump90Count { get; set; }
+
         [Display(Name = "Qopqoq miqodori")]
         public int? CoverCount { get; set; }
+
         [Display(Name = "Jami narxi")]
         public double TotalPrice { get; set; }
     }
